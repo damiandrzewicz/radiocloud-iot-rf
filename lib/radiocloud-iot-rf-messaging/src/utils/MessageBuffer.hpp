413ca7292eff4a9d4563425f8ac8a95d040f6846
Damian Drzewicz <damian.drzewicz@gmail.com>
2022-05-08 22:23:50 +0200

    MessageBuffer implementation + UnitTests

--- lib/radiocloud-iot-rf-messaging/src/utils/MessageBuffer.hpp
+++ lib/radiocloud-iot-rf-messaging/src/utils/MessageBuffer.hpp
@@ -1,15 +1,37 @@
 #pragma once
 
 #include <cstring>
+#include <cstdio>
+#include <stdlib.h>
+//#include <Arduino.h>
+
 
 class MessageBuffer
 {
 public:
     static const uint8_t MaxSize = 100;
 
+    MessageBuffer(const char *delimeter = "|")
+    : delimeter_(delimeter)
+    {
+        clear();
+    }
+
     void appendText(const char *data)
     {
         strcat(buffer_, data);
+    }
+
+    void appendLong(long value)
+    {
+        snprintf(buffer_ + strlen(buffer_), sizeof(buffer_) - strlen(buffer_), "%ld", value);
+    }
+
+    void appendFloat(double value, uint8_t size = 4, uint8_t prec = 2)
+    {
+        char temp[20];
+        dtostrf(value, size, prec, temp);
+        strcat(buffer_, temp);
     }
 
     char *data()
@@ -32,7 +54,29 @@ public:
         return length();
     }
 
+    void clear()
+    {
+        memset(buffer_, 0, sizeof(buffer_));
+    }
+
+    void appendDelimeter()
+    {
+        strcat(buffer_, delimeter_);
+    }
+
+    const char *getDelimeter() const
+    {
+        return delimeter_;
+    }
+
+    MessageBuffer &operator=(const char *str)
+    {
+        clear();
+        strncpy(buffer_, str, sizeof(buffer_));
+        return *this;
+    }
+
 private:
-    
     char buffer_[MaxSize];
+    const char *delimeter_;
 };
--- lib/radiocloud-iot-rf-messaging/test/test_modules/test_MessageBuffer.hpp
+++ lib/radiocloud-iot-rf-messaging/test/test_modules/test_MessageBuffer.hpp
@@ -16,6 +16,7 @@ namespace test_MessageBuffer
 
         // then
         TEST_ASSERT_TRUE(sizeof(obj) > 0);
+        TEST_ASSERT_EQUAL_STRING("|", obj.getDelimeter());
     }
 
     void test_length_emptyBuffer(void) {
@@ -94,6 +95,76 @@ namespace test_MessageBuffer
         TEST_ASSERT_TRUE(obj.isFilled());
     }
 
+    void test_appendLong(void) {
+        // given
+        MessageBuffer obj;
+
+        // when
+        obj.appendLong(4433441);
+
+        // then
+        TEST_ASSERT_EQUAL_STRING("4433441", obj.data());
+    }
+
+    void test_appendFloat(void) {
+        // given
+        MessageBuffer obj;
+
+        // when
+        obj.appendFloat(12345.7755);
+
+        // then
+        TEST_ASSERT_EQUAL_STRING("12345.78", obj.data());
+    }
+
+    void test_customDelimeter(void) {
+        // given
+        MessageBuffer obj("x");
+
+        // when
+        // then
+        TEST_ASSERT_EQUAL_STRING("x", obj.getDelimeter());
+    }
+
+    void test_assignOperator(void) {
+        // given
+        MessageBuffer obj;
+        TEST_ASSERT_EQUAL_STRING("", obj.data());
+
+        // when
+        obj = "someData123";
+        // then
+        TEST_ASSERT_EQUAL_STRING("someData123", obj.data());
+    }
+
+    void test_clear(void) {
+        // given
+        MessageBuffer obj;
+        obj.appendText("initialData");
+        TEST_ASSERT_EQUAL_STRING("initialData", obj.data());
+
+        // when
+        obj.clear();
+        
+        // then
+        TEST_ASSERT_EQUAL_STRING("", obj.data());
+    }
+
+    void test_buildMessage(void) {
+        // given
+        MessageBuffer obj;
+
+        // when
+        obj.appendText("text");
+        obj.appendDelimeter();
+        obj.appendLong(123);
+        obj.appendDelimeter();
+        obj.appendFloat(1.23);
+        
+        // then
+        TEST_ASSERT_EQUAL_STRING("text|123|1.23", obj.data());
+    }
+
     void process()
     {
         RUN_TEST(test_createObject_noArgs);
@@ -104,5 +175,11 @@ namespace test_MessageBuffer
         RUN_TEST(test_length_filledBuffer);
         RUN_TEST(test_data_filledBuffer);
         RUN_TEST(test_isFilled_filledBuffer);
+        RUN_TEST(test_appendLong);
+        RUN_TEST(test_appendFloat);
+        RUN_TEST(test_customDelimeter);
+        RUN_TEST(test_assignOperator);
+        RUN_TEST(test_clear);
+        RUN_TEST(test_buildMessage);
     }
 };
